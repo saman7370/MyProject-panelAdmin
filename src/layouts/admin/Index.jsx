06884456e781efd1d8698317e5b140e8bc2a4fb3
saman7370 <mehralisaman8@gmail.com>
2022-05-08 17:23:06 +0430

    navbar-sidebar

---
+++ src/layouts/admin/Index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './navbar/Navbar';
+import Sidebar from './sidebar/Sidebar';
+
+const Index = () => {
+    return (
+        <div>
+           <Navbar/>
+           <Sidebar/> 
+        </div>
+    );
+}
+
+export default Index;
+
+
+
+
+
---
+++ src/layouts/admin/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Leftcontent from './LeftContent';
+import Rightcontent from './RightContent';
+
+const Navbar = () => {
+    return (
+        <>
+            {/* <!-- #region(collapsed) navbar  --> */}
+            <nav className="navbar fixed-top navbar-dark bg-secondary top_navbar py-0">
+                <div className="container-fluid h-100 pe-0">
+                   <Rightcontent />
+                   <Leftcontent />
+                </div>
+           </nav>
+            </>
+            );
+}
+
+export default Navbar;
