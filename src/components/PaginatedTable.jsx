41f71fc261f4249e1585bc92aa19492ba50b652e
saman7370 <mehralisaman8@gmail.com>
2022-05-18 16:22:50 +0430

    search-component

--- src/components/PaginatedTable.jsx
+++ src/components/PaginatedTable.jsx
@@ -1,88 +1,120 @@
 import React, { useEffect, useState } from 'react';
-let numOFPage = 2;
 
-const Paginatedtable = ({ data, DataInfo, additionFeild}) => {
-  const [tableData , setTableData] = useState([]);
-  const [currentPage , setCurrentPage] = useState(1);
-  const [pages , setPages] = useState([]);
-  const [pageCount , setPageCount] = useState(1);
-  
+
+const Paginatedtable = ({ children, data, DataInfo, additionFeild, searchParams , numOFPage }) => {
+  const [initData, setInitData] = useState(data);
+  const [tableData, setTableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(2);
+  const [pages, setPages] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [searchCarected, setSearchCarected] = useState("");
+
   useEffect(() => {
-    let pCount = Math.ceil(data.length / numOFPage);
+    let pCount = Math.ceil(initData.length / numOFPage);
     setPageCount(pCount);
     let pArr = []
-    for (let i = 1; i<= pCount; i++) pArr = [...pArr, i]
-    setPages(pArr) 
-  }, []);
+    for (let i = 1; i <= pCount; i++) pArr = [...pArr, i]
+    setPages(pArr)
+  }, [initData]);
 
   useEffect(() => {
     let start = (currentPage * numOFPage) - numOFPage
-    let end = (currentPage * numOFPage) 
-    setTableData(data.slice(start , end))
-   
-  }, [currentPage]);
+    let end = (currentPage * numOFPage)
+    setTableData(initData.slice(start, end))
 
-    return (
-        <>
-            <table className="table table-responsive text-center table-hover table-bordered">
-                <thead className="table-secondary">
-                    <tr>
-                        {DataInfo.map(i => (
-                            <th key={i.feild}>{i.title}</th>
-                        ))}
-                        {
-                            additionFeild ? (
-                                <th>{additionFeild.title}</th>
-                            ) :
-                                null
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableData.map(t => (
-                        <tr key={t.id}>
-                            {DataInfo.map(i => (
-                                <td key={i.feild + "_" + t.id}>{t[i.feild]}</td>
-                            ))}
-                            {
-                                additionFeild ? (
-                                    <td>{additionFeild.elements(t.id)}</td>
-                                ) :
-                                    null
-                            }
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <nav
-                aria-label="Page navigation example"
-                className="d-flex justify-content-center"
-            >
-                <ul className="pagination dir_ltr">
-                    <li className="page-item">
-                        <span className={`page-link pointer ${currentPage == 1 ? "disbled" : ""}`}  aria-label="Previous" onClick={()=>setCurrentPage(currentPage - 1)}>
-                            <span aria-hidden="true">&raquo;</span>
-                        </span>
-                    </li>
-                    {
-                       pages.map(page =>(
-                        <li className="page-item" key={page.id}>
-                        <span className={`page-link pointer ${currentPage == page ? "alert-success" : ""}`} onClick={()=>setCurrentPage(page)}>
-                            {page}
-                        </span>
-                        </li>
-                       ))
-                    }
-                    <li className="page-item">
-                        <span className={`page-link pointer ${currentPage == pageCount ? "disbled" : ""}`} aria-label="Next" onClick={()=>setCurrentPage(currentPage + 1)}>
-                            <span aria-hidden="true">&laquo;</span>
-                        </span>
-                    </li>
-                </ul>
-            </nav>
+  }, [currentPage, initData]);
 
-        </>
-    );
+  useEffect(() => {
+    setInitData(data.filter(d => d[searchParams.searchFeild].includes(searchCarected)))
+    setCurrentPage(1)
+  }, [searchCarected]);
+
+  return (
+    <>
+      <div className="row justify-content-between">
+        <div className="col-10 col-md-6 col-lg-4">
+          <div className="input-group mb-3 dir_ltr">
+            <input
+              type="text"
+              className="form-control"
+              placeholder={searchParams.placeholder}
+              onChange={(e) => setSearchCarected(e.target.value)}
+            />
+            <span className="input-group-text">{searchParams.title}</span>
+          </div>
+        </div>
+        <div className="col-2 col-md-6 col-lg-4 d-flex flex-column align-items-end">
+          {children}
+          {/* <Addcategory/> */}
+        </div>
+      </div>
+
+      <table className="table table-responsive text-center table-hover table-bordered">
+        <thead className="table-secondary">
+          <tr>
+            {DataInfo.map(i => (
+              <th key={i.feild}>{i.title}</th>
+            ))}
+            {
+              additionFeild ? (
+                <th>{additionFeild.title}</th>
+              ) :
+                null
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map(t => (
+            <tr key={t.id}>
+              {DataInfo.map(i => (
+                <td key={i.feild + "_" + t.id}>{t[i.feild]}</td>
+              ))}
+              {
+                additionFeild ? (
+                  <td>{additionFeild.elements(t.id)}</td>
+                ) :
+                  null
+              }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {
+        pages.length > 1 ? (
+          <nav
+            aria-label="Page navigation example"
+            className="d-flex justify-content-center"
+          >
+            <ul className="pagination dir_ltr">
+              <li className="page-item">
+                <span className={`page-link pointer ${currentPage == 1 ? "disbled" : ""}`} aria-label="Previous" onClick={() => setCurrentPage(currentPage - 1)}>
+                  <span aria-hidden="true">&raquo;</span>
+                </span>
+              </li>
+              {
+                pages.map(page => (
+                  <li className="page-item" key={page.id}>
+                    <span className={`page-link pointer ${currentPage == page ? "alert-success" : ""}`} onClick={() => setCurrentPage(page)}>
+                      {page}
+                    </span>
+                  </li>
+                ))
+              }
+              <li className="page-item">
+                <span className={`page-link pointer ${currentPage == pageCount ? "disbled" : ""}`} aria-label="Next" onClick={() => setCurrentPage(currentPage + 1)}>
+                  <span aria-hidden="true">&laquo;</span>
+                </span>
+              </li>
+            </ul>
+          </nav>
+
+        ) :
+          null
+      }
+
+
+    </>
+  );
 }
 
 export default Paginatedtable;
--- src/pages/category/CategoryTable.jsx
+++ src/pages/category/CategoryTable.jsx
@@ -1,80 +1,89 @@
 import React from "react";
 import Paginatedtable from "../../components/PaginatedTable";
+import Addcategory from "./AddCategory";
 
 const Categorytable = () => {
   const data = [
     {
-        id:"1",
-        category:"aaa",
-        title:"bbb",
-        price:"111",
-        stock:"5",
-        like_count:"2",
-        status:"1"
+      id: "1",
+      category: "aaa",
+      title: "bbb",
+      price: "111",
+      stock: "5",
+      like_count: "2",
+      status: "1"
     },
     {
-        id:"2",
-        category:"ccc",
-        title:"ddd",
-        price:"222",
-        stock:"7",
-        like_count:"8",
-        status:"2"
+      id: "2",
+      category: "ccc",
+      title: "ddd",
+      price: "222",
+      stock: "7",
+      like_count: "8",
+      status: "2"
     },
     {
-        id:"3",
-        category:"eee",
-        title:"fff",
-        price:"333",
-        stock:"7",
-        like_count:"8",
-        status:"2"
+      id: "3",
+      category: "eee",
+      title: "ddd",
+      price: "333",
+      stock: "7",
+      like_count: "8",
+      status: "2"
     },
     {
-        id:"4",
-        category:"ggg",
-        title:"hhh",
-        price:"444",
-        stock:"7",
-        like_count:"8",
-        status:"2"
+      id: "4",
+      category: "ggg",
+      title: "ddd",
+      price: "444",
+      stock: "7",
+      like_count: "8",
+      status: "2"
     },
     {
-        id:"5",
-        category:"fff",
-        title:"zzz",
-        price:"555",
-        stock:"7",
-        like_count:"8",
-        status:"2"
+      id: "5",
+      category: "fff",
+      title: "zzz",
+      price: "555",
+      stock: "7",
+      like_count: "8",
+      status: "2"
     },
-]
+  ]
 
-const DataInfo = [
-    {feild:"id" , title:"#"},
-    {feild:"title" , title:"عنوان محصول"},
-    {feild:"price" , title:"مبلغ محصول"},
-]
+  const DataInfo = [
+    { feild: "id", title: "#" },
+    { feild: "title", title: "عنوان محصول" },
+    { feild: "price", title: "مبلغ محصول" },
+  ]
 
-const additionElements = (itemId)=>{
-  console.log(itemId)
-  return(
-    <>
-    <i className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip" title="زیرمجموعه" data-bs-toggle="tooltip" data-bs-placement="top"></i>
-    <i className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip" title="ویرایش دسته" data-bs-toggle="modal" data-bs-placement="top" data-bs-target="#add_product_category_modal"></i>
-    <i className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip" title="افزودن ویژگی" data-bs-toggle="modal" data-bs-target="#add_product_category_attr_modal"></i>
-    <i className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip" title="حذف دسته" data-bs-toggle="tooltip" data-bs-placement="top"></i>
-    </>
-  )
-}
+  const additionElements = (itemId) => {
+    console.log(itemId)
+    return (
+      <>
+        <i className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip" title="زیرمجموعه" data-bs-toggle="tooltip" data-bs-placement="top"></i>
+        <i className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip" title="ویرایش دسته" data-bs-toggle="modal" data-bs-placement="top" data-bs-target="#add_product_category_modal"></i>
+        <i className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip" title="افزودن ویژگی" data-bs-toggle="modal" data-bs-target="#add_product_category_attr_modal"></i>
+        <i className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip" title="حذف دسته" data-bs-toggle="tooltip" data-bs-placement="top"></i>
+      </>
+    )
+  }
 
-const additionFeild = {
-  title:"عملیات",
-  elements: (itemId)=>additionElements(itemId)
-}
+  const additionFeild = {
+    title: "عملیات",
+    elements: (itemId) => additionElements(itemId)
+  }
 
-  return (  
-  <Paginatedtable data={data} DataInfo={DataInfo} additionFeild={additionFeild}/>
+  const searchParams = {
+    title : "جستجو",
+    placeholder : "قسمتی از عنوان را وارد کنید",
+    searchFeild: "title"
+  }
+
+  return (
+    <Paginatedtable data={data} DataInfo={DataInfo} additionFeild={additionFeild} searchParams={searchParams} numOFPage={2}>
+      <Addcategory />
+    </Paginatedtable>
   );
 };
 
